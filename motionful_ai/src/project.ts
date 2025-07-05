35163883e3a0d68ccf53baf48cc10e31ab90e77e
trenaudie <tanguy.renaudie@gmail.com>
2025-07-04 21:17:12 -0400

    saving working version of cat

--- motionful_ai/src/project.ts
+++ motionful_ai/src/project.ts
@@ -1,6 +1,6 @@
 import {makeProject} from '@motion-canvas/core';
 
-import example from './scenes/example?scene';
+import example from './scenes2/example9_circle_cat?scene';
 
 export default makeProject({
   scenes: [example],
